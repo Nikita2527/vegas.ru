d03667c75b19dac0af5f7ff714551c8bcb27a6d7
Nikita2527 <91638728+Nikita2527@users.noreply.github.com>
2021-09-30 01:49:44 +0300

    Add files via upload

---
+++ двигатель/страниц/config.php
@@ -0,0 +1,27 @@
+<?php
+	$config = array();
+	$config['mysql_host'] = 'localhost'; 
+	$config['mysql_user'] = 'root';
+	$config['mysql_pass'] = '';
+	$config['mysql_database'] = 'sait';
+	$config['_name_project_'] = 'Received'; // Название проекта
+	$config['_ip_'] = 'localhost'; // IP
+	$config['_port_'] = 7777; // Порт
+	$config['youtube'] = '//' . 'vk.com/matveibunos'; // Адрес на ютуб
+	$config['vk'] = '//' . 'vk.com/matveibunos'; // Адрес на группу
+	$config['forum'] = '//' . 'forum.luxe-rp.ru'; // Адрес на форум
+	$config['table_account'] = 'accounts'; // Название таблицы с аккаунтами
+	$config['field_name'] = 'name'; // Название поля с именем персонажа
+	$config['field_password'] = 'password'; // Название поля с паролем персонажа
+	$config['field_ID'] = 'id'; // Название поля с id персонажа
+	$config['field_level'] = 'level'; // Название поля с уровнем персонажа
+	$config['field_exp'] = 'exp'; // Название поля с количеством очков опыта персонажа
+	$config['field_money'] = 'money'; // Название поля с количеством денег на руках персонажа
+	$config['field_bank'] = 'bank'; // Название поля с количеством денег в банке у персонажа
+	$config['field_gender'] = 'gender'; // Название поля с полом персонажа
+	$config['field_house'] = 'house'; // Название поля с домом персонажа
+	$config['field_business'] = 'business'; // Название поля с бизнесом персонажа 
+	$config['field_hotel'] = 'hotel'; // Название поля с отелем персонажа
+	$config['field_warns'] = 'warns'; // Название поля с количеством предупреждений персонажа
+	$config['field_donate'] = 'donate'; // Название поля с количеством доната персонажа
+?>
